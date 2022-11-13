7f73bc8346a3c864ee4d57472426661ce653e716
Galan Gahara Triguna Toding <galan.gahara@ui.ac.id>
2022-11-13 16:22:44 +0700

    gatau lagi

--- main/tests.py
+++ main/tests.py
@@ -1,8 +1,3 @@
 from django.test import LiveServerTestCase, TestCase, tag
 from django.urls import reverse
 from selenium import webdriver
-
-
-@tag('functional')
-class FunctionalTestCase(LiveServerTestCase):
-    
